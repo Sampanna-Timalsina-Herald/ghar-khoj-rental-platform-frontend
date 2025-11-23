92f5d7579ec2ba585d840db9e12fe3ced41a1a36
Sampanna Timalsina <np03cs4a230406@heraldcollege.edu.np>
2025-11-23 13:58:34 +0545

    Edited image in Register Page

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import modernInterior from '../../assets/modern-interior1.webp';
+import modernInterior from '../../assets/interior1.jpg';
 import { 
   Eye, 
   EyeOff, 
