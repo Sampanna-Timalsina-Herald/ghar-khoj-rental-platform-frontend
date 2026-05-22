a36f4f8754aef65d931567f62436efd85c193784
Sampanna Timalsina <np03cs4a230406@heraldcollege.edu.np>
2026-05-22 16:41:02 +0545

    Update notifications API

--- src/utils/notificationsApi.js
+++ src/utils/notificationsApi.js
@@ -1,46 +1,39 @@
-import axios from 'axios';
-
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
-const api = axios.create({
-  baseURL: `${API_URL}/notifications`,
-  withCredentials: true,
-});
+import api from '../api/axios';
 
 export const notificationsApi = {
   // Get all notifications
   getNotifications: async () => {
-    const response = await api.get('/');
+    const response = await api.get('/notifications');
     return response.data;
   },
 
   // Get unread count
   getUnreadCount: async () => {
-    const response = await api.get('/unread-count');
+    const response = await api.get('/notifications/unread-count');
     return response.data;
   },
 
   // Mark as read
   markAsRead: async (notificationId) => {
-    const response = await api.put(`/${notificationId}/read`);
+    const response = await api.put(`/notifications/${notificationId}/read`);
     return response.data;
   },
 
   // Mark all as read
   markAllAsRead: async () => {
-    const response = await api.put('/mark-all-read');
+    const response = await api.put('/notifications/mark-all-read');
     return response.data;
   },
 
   // Delete notification
   deleteNotification: async (notificationId) => {
-    const response = await api.delete(`/${notificationId}`);
+    const response = await api.delete(`/notifications/${notificationId}`);
     return response.data;
   },
 
   // Delete all notifications
   deleteAll: async () => {
-    const response = await api.delete('/');
+    const response = await api.delete('/notifications');
     return response.data;
   },
 };
